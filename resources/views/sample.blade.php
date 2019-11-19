41e2b3ed8c0246aaa9f2c811c8462b1e738e526b
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-19 12:46:01 +0900

    Hellocontrollerを作成

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,8 @@
+<DOCTYPE!>
+  <html charset="UTF-8">
+    <head>
+    </head>
+    <body>
+      <p>Hello</p>
+    </body>
+  </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function() {
+    return view('sample');
+});
+
+Route::get('hello/{msg?}', function ($msg) {
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {font-size:16px; text-align:right; color:#eee;
+        margin: -40px 0px -50px 0px; }
+        </style>
+        </head>
+    <body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これはサンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+    return $html;
+
+});
