340b1482f4f58a73ffbbd853469b54c41da29456
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-20 16:48:22 +0900

    ビューコンポーザを利用

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,7 +8,10 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-  <p>必要なだけ記述できます。</p>
+  <p>Controller value<br>'message' = {{$}}
+  <ul>
+  @each('components.item', $data, 'item')
+  </ul>
 @endsection
 
 @section('footer')
