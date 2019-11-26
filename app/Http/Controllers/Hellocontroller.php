b2e43e5b5d023ef0533af012923a0a1e52a1fdd0
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-26 18:11:23 +0900

    モデルPerson、コンローラーPersonControllerを作成

--- app/Http/Controllers/Hellocontroller.php
+++ app/Http/Controllers/Hellocontroller.php
@@ -199,12 +199,17 @@ class Hellocontroller extends Controller
     // }
 
     public function index(Request $request) {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        // if (isset($request->id)) {
+        //     $param = ['id' => $request->id];
+        //     $items = DB::select('select * from people where id = :id', $param);
+        // } else {
+        //     $items = DB::select('select * from people');
+        // }
+        // return view('hello.index', ['items' => $items]);
+        // $items = DB::table('people')->get();
+        // return view('hello.index', ['items' => $items]);
+
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('hello.index', ['items' => $items]);
     }
 
@@ -218,14 +223,17 @@ class Hellocontroller extends Controller
             'mail' => $request->mail,
             'age'  => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values(:name, :mail, :age)', $param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
     }
 
     public function edit(Request $request) {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        // $param = ['id' => $request->id];
+        // $item = DB::select('select * from people where id = :id', $param);
+        // return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+        ->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request) {
@@ -235,19 +243,47 @@ class Hellocontroller extends Controller
             'mail' => $request->mail,
             'age'  => $request->age,
         ];
-        DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+        // DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+        // return redirect('/hello');
+        DB::table('people')
+        ->where('id', $request->id)
+        ->update($param);
         return redirect('/hello');
     }
 
     public function del(Request $request) {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        // $param = ['id' => $request->id];
+        // $item = DB::select('select * from people where id = :id', $param);
+        // return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')
+        ->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
     }
 
     public function remove(Request $request) {
-        $param = ['id' => $request->id];
-        DB::delete('delete from people where id = :id', $param);
+        // $param = ['id' => $request->id];
+        // DB::delete('delete from people where id = :id', $param);
+        // return redirect('/hello');
+        DB::table('people')
+        ->where('id', $request->id)->delete();
         return redirect('/hello');
+    }
+
+    public function show(Request $request) {
+        // $id = $request->id;
+        // $items = DB::table('people')->where('id', '<=', $id)->get();
+        // return view('hello.show', ['items' => $items]);
+        // $name = $request->name;
+        // $items = DB::table('people')
+        // ->where('name', 'like', '%' . $name . '%')
+        // ->orWhere('mail', 'like', '%' . $name . '%')
+        // ->get();
+        // return view('hello.show', ['items' => $items]);
+
+        $min = $request->min;
+        $max = $request->max;
+        $items = DB::table('people')
+        ->whereRaw('age >= ? and age <= ?', [$min, $max])->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -5,6 +5,8 @@
       body {font-size:16pt; color:#999; margin: 5px;}
       h1 {font-size: 50px; text-align: right; color: #f6f6f6;
       margin: -20px 0px -30px 0px; letter-spacing: -4pt;}
+      th { background-color:#999; color:fff; padding:5px 10px; }
+      td { border: solid 1px #aaa; color:#999; padding:5px 10px; }
     </style>
   </head>
   <body>
